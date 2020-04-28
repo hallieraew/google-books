8236b3aa6a01d9b94c9597133647c12fbf888735
hallieraew <hallieweintraub@gmail.com>
2020-04-28 16:13:25 -0500

    updating routes and stuff

--- google-books-react-app/server.js
+++ google-books-react-app/server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-// const apiRoutes = require("./routes/api");
+const apiRoutes = require("./routes/api");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Use apiRoutes
-// app.use("/api", apiRoutes);
+app.use("/api", apiRoutes);
 // 
 // Send every request to the React app
 // Define any API routes before this runs
